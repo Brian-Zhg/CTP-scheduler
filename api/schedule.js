f82a24d99d2181048d81bf01c3fb3d6079ad08ec
Axel Mizerovskiy <smizerovskiy@gmail.com>
2026-08-27 13:58:20 -0400

    relational dates

--- api/schedule.js
+++ api/schedule.js
@@ -35,7 +35,6 @@ module.exports = async function handler(req, res) {
               }
             ]
         `;
-
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent?key=${apiKey}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
